9b59fa63548c481787572015b954441bb3d8516d
theBashShell <edohbruno@gmail.com>
2020-02-14 03:36:03 +0000

    rust guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter Guessed Number: ");
+    let mut guess = String::new();
+    
+    io::stdin().read_line(&mut guess).expect("Failed");
+    
+    println!("You guessed {}", guess);
 }

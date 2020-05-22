bb29f674c63dd16ecf0d2daf8afa12d1adc65ec0
theBashShell <edohbruno@gmail.com>
2020-05-22 13:51:33 +0000

    arrays + tuples

--- better/src/main.rs
+++ better/src/main.rs
@@ -14,12 +14,16 @@ fn main() {
 
     let num = rand::thread_rng().gen_range(0, 301);
 
-    const PI : f64 = 3.14;
+    const PI: f64 = 3.14;
 
     let a = "go".to_uppercase();
 
-    let tup : (char, i8) = ('c', 10);
-    let array : [i32; 3] = [1, 2, 3];
+    let tup: (char, i8) = ('c', 10);
+    let array: [i32; 3] = [1, 2, 3];
+    let arr = [0; 3];
 
-    println!("You guessed: {}--{}::{}, {} {}", num, PI, a, tup.0, array[2]);
+    println!(
+        "You guessed: {}--{}::{}, {} {} {}",
+        num, PI, a, tup.0, array[2], arr[2]
+    );
 }

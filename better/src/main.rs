f2d9ced288565b7b210f81ba3706c16686afc42b
theBashShell <edohbruno@gmail.com>
2020-05-22 11:36:21 +0000

    randomness

--- better/src/main.rs
+++ better/src/main.rs
@@ -1,15 +1,18 @@
-use std::io;
+// use std::io::stdin;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    // println!("Guess the number!");
 
-    println!("Please input your guess.");
+    // println!("Please input your guess.");
 
-    let mut guess = String::new();
+    // let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    // stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    let num = rand::thread_rng().gen_range(0, 301);
+
+    println!("You guessed: {}", num);
 }

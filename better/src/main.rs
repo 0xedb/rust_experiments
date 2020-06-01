08f7bda7c7033676770d902c8d3cd0029b351aff
theBashShell <edohbruno@gmail.com>
2020-06-01 00:32:56 +0000

    array tuples and types

--- better/src/main.rs
+++ better/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let status  = true;
     let age : i128 = 0o2323111;
+    let tup : (i8, bool) = (9, false);
+    let arr = [9; 5];
+
     println!("hello world {} {}", age, status);
+    println!("{} {}", tup.0, tup.1);
+    println!("{}", arr[3]);
 }

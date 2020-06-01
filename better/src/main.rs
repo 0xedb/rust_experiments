839125363bcd35462ce59c944ec71d3ae874aea5
theBashShell <edohbruno@gmail.com>
2020-06-01 00:40:58 +0000

    more functions

--- better/src/main.rs
+++ better/src/main.rs
@@ -7,4 +7,21 @@ fn main() {
     println!("hello world {} {}", age, status);
     println!("{} {}", tup.0, tup.1);
     println!("{}", arr[3]);
+    println!("{}", add_two(10, 39));
+
+    println!("{}", checker(99));
+}
+
+fn add_two(first : i16, second : i16) -> i16 {
+    first + second
+}
+
+fn checker(num : i8) -> i8 {
+    let y =     if num > 10 {
+        num + 1
+    } else {
+        num - 1
+    };
+
+    y
 }

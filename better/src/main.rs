06834e05eebe750ebaa45948db4c7ce195f63265
theBashShell <edohbruno@gmail.com>
2020-06-04 22:44:17 +0000

    rust

--- better/src/main.rs
+++ better/src/main.rs
@@ -1,39 +1,24 @@
 fn main() {
-    let status  = true;
-    let age : i128 = 0o2323111;
-    let tup : (i8, bool) = (9, false);
-    let arr = [9; 5];
+    let mut v = 13;
 
-    println!("hello world {} {}", age, status);
-    println!("{} {}", tup.0, tup.1);
-    println!("{}", arr[3]);
-    println!("{}", add_two(10, 39));
+    struct Human {
+        name : String,
+        age : i16,
+    }
 
-    println!("{}", checker(99));
-    println!("{} here", looper(3));
-}
 
-fn add_two(first : i16, second : i16) -> i16 {
-    first + second
-}
-
-fn checker(num : i8) -> i8 {
-    let y =     if num > 10 {
-        num + 1
-    } else {
-        num - 1
+    println!("{}", v);
+    borrower(&mut v);
+    println!("{}", v);
+ 
+    let bruno = Human {
+        name : String::from("bruno edoh"),
+        age : 39
     };
 
-    y
+    println!("{}", bruno.name);
 }
 
-fn looper(mut num: i8) -> i8 {
-    loop {
-        println!("{}", num);
-        num = num + 1;
-
-        if num == 20 {
-            break num
-        }
-    }
+fn borrower(i : &mut i8) {
+    *i = *i + 11;
 }
